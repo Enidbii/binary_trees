becacdbd744aadf5581cbce550d195603b16d3e4
EnidBii <chebsenid@gmail.com>
2023-06-02 12:54:45 -0700

    changes

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,13 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
-#include stdlib.h
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
