c1d6592e5066326b18ef33184013527182e0212f
EnidBii <chebsenid@gmail.com>
2023-06-02 13:36:49 -0700

    changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,7 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lt, rt;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 
 	lt = binary_tree_height(tree->left);
