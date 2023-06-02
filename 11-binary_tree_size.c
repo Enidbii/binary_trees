fd2d62dacbbc29d41c761f00fd1e51a11ef141a7
EnidBii <chebsenid@gmail.com>
2023-06-02 13:14:18 -0700

    changes

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,8 +12,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	lt = binary_tree_size(tree->lt);
-	rt = binary_tree_size(tree->rt);
+	lt = binary_tree_size(tree->left);
+	rt = binary_tree_size(tree->right);
 
 	return (1 + lt + rt);
 }
